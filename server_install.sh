cefb2867e84281c51316724ab74489840964b4e4
Ovidiu S Bokar <bovidiu87@gmail.com>
2014-08-07 11:13:34 +0100

    Core

---
+++ server_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# A simple way to install all Apache2, Mysql, PHP and dependencies
+# Author : Ovidiu S Bokar
+# Web : www.osbwebsolutions.com
+# Email : contact@osbwebsolutions.com
+
+
+apt-get update
+apt-get upgrade
+
+apt-get install apache2
+
+apt-get install mysql-server
+
+apt-get install php5
+apt-get install php5-mysql
+apt-get install php5-cli
+apt-get install libapache2-mod-php5 
+apt-get install php5-mcrypt
+
+
+/etc/init.d/apache2 restart
+
+echo 'Thank you for using my script.';
+
+firefox http://www.osbwebsolutions.com
